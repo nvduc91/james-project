4882d355c6e06a0a7ad501dd18f1433de337a009
ducnv <duc91.nvd@gmail.com>
2020-04-10 17:26:36 +0700

    JAMES-3135: fix linebreak in DumbBlobStoreCache

--- server/blob/blob-cassandra/src/main/java/org/apache/james/blob/cassandra/cache/DumbBlobStoreCache.java
+++ server/blob/blob-cassandra/src/main/java/org/apache/james/blob/cassandra/cache/DumbBlobStoreCache.java
@@ -25,8 +25,10 @@ import org.reactivestreams.Publisher;
 
 public interface DumbBlobStoreCache {
     Publisher<Void> cache(BlobId blobId, byte[] data);
+
     Publisher<Void> cache(BlobId blobId, InputStream data);
 
     Publisher<byte[]> read(BlobId blobId);
+
     Publisher<Void> remove(BlobId blobId);
 }
