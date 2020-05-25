f6fdd320eac65f7212837b63b61552df2ab84917
duc91 <duc91.nvd@gmail.com>
2020-05-25 17:28:55 +0700

    fixup! JAMES-3093 Add JWT Authentication, integration test for JMAP RFC 8621

--- server/protocols/jmap-rfc-8621-integration-tests/distributed-jmap-rfc-8621-integration-tests/src/test/java/org/apache/james/jmap/rfc8621/distributed/DistributedJWTAuthenticationTest.java
+++ server/protocols/jmap-rfc-8621-integration-tests/distributed-jmap-rfc-8621-integration-tests/src/test/java/org/apache/james/jmap/rfc8621/distributed/DistributedJWTAuthenticationTest.java
@@ -38,7 +38,7 @@ public class DistributedJWTAuthenticationTest implements JWTAuthenticationContra
         CassandraRabbitMQJamesConfiguration.builder()
             .workingDirectory(tmpDir)
             .configurationFromClasspath()
-            .blobStore(BlobStoreConfiguration.objectStorage())
+            .blobStore(BlobStoreConfiguration.objectStorage().disableCache())
             .build())
         .extension(new DockerElasticSearchExtension())
         .extension(new CassandraExtension())
--- server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/JWTAuthenticationContract.scala
+++ server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/JWTAuthenticationContract.scala
@@ -18,45 +18,40 @@
  ****************************************************************/
 package org.apache.james.jmap.rfc8621.contract
 
-import io.restassured.RestAssured.{`given`, `with`, requestSpecification}
-import io.restassured.http.ContentType
+import io.restassured.RestAssured.{`given`, requestSpecification}
+import io.restassured.http.Header
+import org.apache.http.HttpStatus._
 import org.apache.james.GuiceJamesServer
+import org.apache.james.core.Username
 import org.apache.james.jmap.rfc8621.contract.Fixture._
-import org.apache.james.jmap.rfc8621.contract.tags.CategoryTags
-import org.hamcrest.Matchers.{both, equalTo, isA, notNullValue}
-import org.junit.jupiter.api.{BeforeEach, Tag, Test}
 import org.apache.james.jmap.rfc8621.contract.JWTAuthenticationContract._
+import org.apache.james.jmap.rfc8621.contract.tags.CategoryTags
 import org.apache.james.utils.DataProbeImpl
+import org.junit.jupiter.api.{BeforeEach, Tag, Test}
 
 object JWTAuthenticationContract {
-  private def fromGoodContinuationTokenRequest = `with`
-        .contentType(ContentType.JSON)
-        .accept(ContentType.JSON)
-        .body(s"""{
-          | "username": "${BOB.getLocalPart}",
-          | "clientName": "Mozilla Thunderbird",
-          | "clientVersion": "42.0",
-          | "deviceName": "Joe Blogg’s iPhone"
-          | }""".stripMargin)
-      .post("/authentication")
-        .body
-      .path("continuationToken")
-        .toString
+  private val USER_TOKEN: String =
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyQGRvbWFpbi50bGQifQ.U-dUPv6OU6KO5N7CooHUfMkCd" +
+      "FJHx2F3H4fm7Q79g1BPfBSkifPj5xyVlZ0JwEGXypC4zBw9ay3l4DxzX7D_6p1Hx_ihXsoLx1Ca-WUo44x-XRSpPfgxiZjHCJkGBLMV3RZlA" +
+      "jip-d18mxkcX3JGplX_sCQkFisduAOAHuKSUg9wI6VBgUQi_0B35FYv6tP_bD6eFtvaAUN9QyXXh8UQjEp8CO12lRz6enfLx_V6BG_fEMkee" +
+      "6vRqdEqx_F9OF3eWTe1giMp_JhQ7_l1OXXtbd4TndVvTeuVy4irPbsRc-M8x_-qTDpFp6saRRsyOcFspxPp5n3yIhEK7B3UZiseXw"
 
-  private def fromGoodAccessTokenRequest(continuationToken: String) = `with`
-    .contentType(ContentType.JSON).accept(ContentType.JSON)
-    .body(
-      s"""{
-         |  "token": "${continuationToken}",
-         |  "method": "password",
-         |  "password": "$BOB_PASSWORD}"
-         | }""".stripMargin)
-    .post("/authentication").path("accessToken")
-    .toString
+  private val UNKNOWN_USER_TOKEN: String =
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIn0.T04BTk" +
+      "LXkJj24coSZkK13RfG25lpvmSl2MJ7N10KpBk9_-95EGYZdog-BDAn3PJzqVw52z-Bwjh4VOj1-j7cURu0cT4jXehhUrlCxS4n7QHZD" +
+      "N_bsEYGu7KzjWTpTsUiHe-rN7izXVFxDGG1TGwlmBCBnPW-EFCf9ylUsJi0r2BKNdaaPRfMIrHptH1zJBkkUziWpBN1RNLjmvlAUf49" +
+      "t1Tbv21ZqYM5Ht2vrhJWczFbuC-TD-8zJkXhjTmA1GVgomIX5dx1cH-dZX1wANNmshUJGHgepWlPU-5VIYxPEhb219RMLJIELMY2qN" +
+      "OR8Q31ydinyqzXvCSzVJOf6T60-w"
 
-  private def goodDeleteAccessTokenRequest(accessToken: String): Unit = {
-    `with`.header("Authorization", accessToken).delete("/authentication")
-  }
+  private val INVALID_JWT_TOKEN: String =
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbkBvcGVu" +
+      "thispartiscompletelywrong.reQc3DiVvbQHF08oW1qOUyDJyv3tfzDNk8jhVZequiCdOI9vXnRlOe" +
+      "-yDYktd4WT8MYhqY7MgS-wR0vO9jZFv8ZCgd_MkKCvCO0HmMjP5iQPZ0kqGkgWUH7X123tfR38MfbCVAdPDba-K3MfkogV1xvDhlkPScFr_6MxE" +
+      "xtedOK2JnQZn7t9sUzSrcyjWverm7gZkPptkIVoS8TsEeMMME5vFXe_nqkEG69q3kuBUm_33tbR5oNS0ZGZKlG9r41lHBjyf9J1xN4UYV8n866d" +
+      "a7RPPCzshIWUtO0q9T2umWTnp-6OnOdBCkndrZmRR6pPxsD5YL0_77Wq8KT_5__fGA"
+
+  private val USER: Username = Username.fromLocalPartWithDomain("user", DOMAIN)
+  private val USER_PASSWORD: String = "user"
 }
 
 trait JWTAuthenticationContract {
@@ -65,70 +60,45 @@ trait JWTAuthenticationContract {
     server.getProbe(classOf[DataProbeImpl])
       .fluent()
       .addDomain(DOMAIN.asString())
-      .addUser(BOB.asString(), BOB_PASSWORD)
+      .addUser(USER.asString(), USER_PASSWORD)
 
     requestSpecification = baseRequestSpecBuilder(server).build
   }
 
   @Tag(CategoryTags.BASIC_FEATURE)
   @Test
-  def getMustReturnEndpointsWhenValidAuthorizationHeader(): Unit = {
-    val continuationToken = fromGoodContinuationTokenRequest
-    val token = fromGoodAccessTokenRequest(continuationToken)
-    `given`
-        .header("Authorization", token)
-      .when
-        .get("/authentication").`then`
-      .statusCode(200)
-        .body("api", equalTo("/jmap"))
-        .body("eventSource", both(isA(classOf[String])).and(notNullValue))
-        .body("upload", equalTo("/upload"))
-        .body("download", equalTo("/download"))
-  }
-
-  @Tag(CategoryTags.BASIC_FEATURE)
-  @Test
   def getMustReturnEndpointsWhenValidJwtAuthorizationHeader(): Unit = {
-    val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyQGRvbWFpbi50bGQifQ.U-dUPv6OU6KO5N7CooHUfMkCd" +
-      "FJHx2F3H4fm7Q79g1BPfBSkifPj5xyVlZ0JwEGXypC4zBw9ay3l4DxzX7D_6p1Hx_ihXsoLx1Ca-WUo44x-XRSpPfgxiZjHCJkGBLMV3RZlA" +
-      "jip-d18mxkcX3JGplX_sCQkFisduAOAHuKSUg9wI6VBgUQi_0B35FYv6tP_bD6eFtvaAUN9QyXXh8UQjEp8CO12lRz6enfLx_V6BG_fEMkee" +
-      "6vRqdEqx_F9OF3eWTe1giMp_JhQ7_l1OXXtbd4TndVvTeuVy4irPbsRc-M8x_-qTDpFp6saRRsyOcFspxPp5n3yIhEK7B3UZiseXw"
     `given`
-        .header("Authorization", "Bearer " + token)
-      .when
-        .get("/authentication")
-      .`then`
-        .statusCode(200)
+      .headers(getHeadersWith(new Header(AUTHORIZATION_HEADER, s"Bearer $USER_TOKEN")))
+      .body(ECHO_REQUEST_OBJECT)
+    .when
+      .post()
+    .`then`
+      .statusCode(SC_OK)
   }
 
   @Test
   @Tag(CategoryTags.BASIC_FEATURE)
-  def getMustReturnEndpointsWhenValidUnkwnonUserJwtAuthorizationHeader(): Unit = {
-    val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1bmtub3duQGRvbWFpbi50bGQifQ.hr8AhlNIpiA3Mv_A5ZLyL" +
-      "f1BHeBSaRDfdR_GLV_hlPdIrWv1xtwjBH86E1YnTPx2tTpr_NWTbHcR1OCkuVCpgloEnUNbE3U2l0WrGOX2Eh9dWCXOCtrNvCeSHQuvx5_8W" +
-      "nSVENYidk7o2icE8_gz_Giwf0Z3bHJJYXfAxupv__tCkmhqt3E888VZPjs26AsqxQ29YyX0Fjx8UwKbPrH5-tnyftX-kLjjZNtahVIVtbW4v" +
-      "b8rEEZ4nzqxHqtI2co6yCXjgyoFMdDAKCOU-Bq35Gdo-Qiu8l7a0kQGhuhkjoaVWvw4bcSvunxAnh_0W5g3Lw-rwljNu2JrJS0gAH6NDA"
-    `given`
-        .header("Authorization", "Bearer " + token)
-      .when
-        .get("/authentication")
-      .`then`
-        .statusCode(200)
+  def getMustReturnOKWhenValidUnknownUserJwtAuthorizationHeader(): Unit = {
+    val authHeader: Header = new Header(AUTHORIZATION_HEADER, s"Bearer $UNKNOWN_USER_TOKEN")
+    `given`()
+      .headers(getHeadersWith(authHeader))
+      .body(ECHO_REQUEST_OBJECT)
+    .when()
+      .post()
+    .then
+      .statusCode(SC_UNAUTHORIZED)
   }
 
   @Test
   @Tag(CategoryTags.BASIC_FEATURE)
   def getMustReturnBadCredentialsWhenInvalidJwtAuthorizationHeader(): Unit = {
-    val token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIn0.T04BTk" +
-      "LXkJj24coSZkK13RfG25lpvmSl2MJ7N10KpBk9_-95EGYZdog-BDAn3PJzqVw52z-Bwjh4VOj1-j7cURu0cT4jXehhUrlCxS4n7QHZ" +
-      "EN_bsEYGu7KzjWTpTsUiHe-rN7izXVFxDGG1TGwlmBCBnPW-EFCf9ylUsJi0r2BKNdaaPRfMIrHptH1zJBkkUziWpBN1RNLjmvlAUf" +
-      "49t1Tbv21ZqYM5Ht2vrhJWczFbuC-TD-8zJkXhjTmA1GVgomIX5dx1cH-dZX1wANNmshUJGHgepWlPU-5VIYxPEhb219RMLJIELMY2" +
-      "qNOR8Q31ydinyqzXvCSzVJOf6T60-w"
     `given`
-        .header("Authorization", "Bearer " + token)
-      .when
-        .get("/authentication")
-      .`then`
-        .statusCode(401)
+      .headers(getHeadersWith(new Header(AUTHORIZATION_HEADER, s"Bearer $INVALID_JWT_TOKEN")))
+      .body(ECHO_REQUEST_OBJECT)
+    .when
+      .post()
+    .`then`
+      .statusCode(SC_UNAUTHORIZED)
   }
 }
